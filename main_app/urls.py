ff21deb7883831c3c3f991515a3734eaf153eac7
Tal <trimoni8@gmail.com>
2022-11-03 16:53:53 -0400

    add images

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('cards/', views.cards_index, name='cards_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,20 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+class Card:
+  def __init__(self, name, type, value, pack):
+    self.name = name
+    self.type = type
+    self.value = value
+    self.pack = pack
+
+cards = [
+  Card('Pikachu', 'Electric', 'Common', 'Jungle Pack'),
+  Card('Charizard', 'Fire', 'Super Rare', 'Base Pack'),
+  Card('Mewtwo', 'Psychic', 'Super Rare', 'Base Pack'),
+  Card('Snorlax', 'Normal', 'Rare', 'Jungle Pack')
+]
+
+def cards_index(request):
+  return render(request, 'cards/index.html', { 'cards': cards })
